91d95518d74962b278de2ca3c33823649f181d7a
Salbei <karsus3@web.de>
2018-03-19 14:38:23 +0100

    Fixes

--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -2,9 +2,9 @@
 *   Legt allgemeine Information über die Mission fest.
 */
 
-author = "Name für Gruppe Adler";                                               // Missionsersteller
-onLoadName = "COOP TEMPLATE";                                                   // Name der Mission
-onLoadMission = "";                                                             // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+author = "Slant & Salbei für Gruppe Adler";                                     // Missionsersteller
+onLoadName = "FUNOP ADLER GRANDPRIX";                                           // Name der Mission
+onLoadMission = "Time to have some fun!";                                       // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\loadpic.paa";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
 overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
